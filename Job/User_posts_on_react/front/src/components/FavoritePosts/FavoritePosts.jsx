f45261337784fb814a67afb022a7c751f55d1fbc
DemonInFire <vasili_setyn_mail_ru@mail.ru>
2020-09-18 16:56:30 -0400

    v2.4

--- Job/User_posts_on_react/front/src/components/FavoritePosts/FavoritePosts.jsx
+++ Job/User_posts_on_react/front/src/components/FavoritePosts/FavoritePosts.jsx
@@ -1,123 +1,136 @@
-import React, {useContext, useState} from 'react'
-import {FavoritePostsContext} from './../../context/favoritePostsContext'
-import FavoritePost from './../FavoritePost/FavoritePost'
-import { ModalContext } from '../../context/modalPostInfoContext'
-import Modal from '../ModalWindow/Modal'
-import { CurrentPostContext } from '../../context/currentPostContext'
-import { ModalAddPostContext } from '../../context/modalAddInfoContext'
-import style from './FavoritePosts.module.css'
-import * as firebase from 'firebase'
+import React, { useContext, useState, useEffect } from "react";
+import { FavoritePostsContext } from "./../../context/favoritePostsContext";
+import FavoritePost from "./../FavoritePost/FavoritePost";
+import { ModalContext } from "../../context/modalPostInfoContext";
+import Modal from "../ModalWindow/Modal";
+import { CurrentPostContext } from "../../context/currentPostContext";
+import { ModalAddPostContext } from "../../context/modalAddInfoContext";
+import style from "./FavoritePosts.module.css";
+import * as firebase from "firebase";
 
 const FavoritePosts = () => {
-    const { favoritePosts } = useContext(FavoritePostsContext)
-    const { toggleModal, isModalOpen } = useContext(ModalContext)
-    const { state } = useContext(CurrentPostContext)
-    const { isAddPostModalOpen, toggleAddPostModalOpen } = useContext(ModalAddPostContext)
+  const { favoritePosts } = useContext(FavoritePostsContext);
+  const { toggleModal, isModalOpen } = useContext(ModalContext);
+  const { state } = useContext(CurrentPostContext);
+  const { isAddPostModalOpen, toggleAddPostModalOpen } = useContext(
+    ModalAddPostContext
+  );
 
-    let initialState = {
-        title: '',
-        content: '',
-    }
+  let initialState = {
+    title: "",
+    content: "",
+  };
 
-    const validStatus = true
-    const loaderState = false
+  const validStatus = true;
+  const loaderState = false;
+  const successState = false;
 
-    const [newPostInfo, setNewPostInfo] = useState(initialState)
-    const [isPostValid, setPostValid] = useState(validStatus)
-    const [isLoader, setLoader] = useState(loaderState)
+  const [newPostInfo, setNewPostInfo] = useState(initialState);
+  const [isPostValid, setPostValid] = useState(validStatus);
+  const [isLoader, setLoader] = useState(loaderState);
+  const [isSuccess, setSuccess] = useState(successState);
 
-    const changeHandler = (e) => {
-        setNewPostInfo({...newPostInfo, [e.target.id]:e.target.value})
-        newPostInfo.title.length > 1 && newPostInfo.content.length > 1 ? setPostValid(false) : setPostValid(true)
-    }
+  const changeHandler = (e) => {
+    setNewPostInfo({ ...newPostInfo, [e.target.id]: e.target.value });
+    newPostInfo.title.length > 1 && newPostInfo.content.length > 1
+      ? setPostValid(false)
+      : setPostValid(true);
+  };
 
-    const createPost = () => {
-        (async () => {
-            setLoader(true)
-            let db = await firebase.database()
-            let ref = await db.ref('server/saving-data/user-posts')
-            ref.set({newPostInfo})
-            setLoader(false)
-        })()
-        
-        setNewPostInfo(initialState)
-        setPostValid(validStatus)
-    }
+  const createPost = async () => {
+    setLoader(true);
+    let db = await firebase.database();
+    let ref = await db.ref("server/saving-data/user-posts");
+    ref.set({ newPostInfo });
+    // setSuccess(true)
+    setNewPostInfo(initialState);
+    setPostValid(validStatus);
+  };
 
-    return (
-        <>
-            <div className={style.Container}>
-                {
-                    favoritePosts.map (post => {
-                        return (<FavoritePost post={post} key={post.id}/>)
-                    })
-                }
-            </div>
-            { isModalOpen &&
-                <Modal>
-                    <div className={style.ModalContainer}>
-                        <button className={style.ModalButton} onClick={toggleModal}>x</button>
-                        <h2 className={style.ModalTitle}>{state.title}</h2>
-                        <div className={style.ModalContent}>{state.content}</div>
-                    </div>
-                </Modal>
-            }
-            { isAddPostModalOpen &&
-                <Modal>
-                    <div className={style.ModalContainer}>
-                        <button className={style.ModalButton} onClick={toggleAddPostModalOpen}>x</button>
-                        {isLoader ? (
-                            <div className={style.Loader}></div>
-                        ) : (
-                            <div className={style.Content}>
-                                <div>
-                                    <div>
-                                        <label>Title</label>
-                                    </div>
-                                <div>
-                                    <input 
-                                        type="text" 
-                                        id="title" 
-                                        name="title"
-                                        value={newPostInfo.title} 
-                                        placeholder="Input post title" 
-                                        onChange={changeHandler}
-                                        required
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <label>Content</label>
-                                </div>
-                                <div className={style.TextArea}>
-                                    <textarea 
-                                        id="content" 
-                                        name="content" 
-                                        placeholder="Input post content"
-                                        value={newPostInfo.content} 
-                                        rows="6" 
-                                        onChange={changeHandler}
-                                        required 
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <input 
-                                    type="submit" 
-                                    value="Submit" 
-                                    onClick={createPost}
-                                    disabled={isPostValid}
-                                />
-                            </div>
-                        </div>
-                        )
-                        }
-                    </div>
-                </Modal>
-            }
-        </>
-    )
-}
+  return (
+    <>
+      <div className={style.Container}>
+        {favoritePosts.map((post) => {
+          return <FavoritePost post={post} key={post.id} />;
+        })}
+      </div>
+      {isModalOpen && (
+        <Modal>
+          <div className={style.ModalContainer}>
+            <button className={style.ModalButton} onClick={toggleModal}>
+              x
+            </button>
+            <h2 className={style.ModalTitle}>{state.title}</h2>
+            <div className={style.ModalContent}>{state.content}</div>
+          </div>
+        </Modal>
+      )}
+      {isAddPostModalOpen && (
+        <Modal>
+          <div className={style.ModalContainer}>
+            <button
+              className={style.ModalButton}
+              onClick={toggleAddPostModalOpen}
+            >
+              x
+            </button>
+            {isLoader ? (
+              <div>
+                {isSuccess ? (
+                  <h1>Data successfully saved!</h1>
+                ) : (
+                  <div className={style.Loader}></div>
+                )}
+              </div>
+            ) : (
+              <div className={style.Content}>
+                <div>
+                  <div>
+                    <label>Title</label>
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      id="title"
+                      name="title"
+                      value={newPostInfo.title}
+                      placeholder="Input post title"
+                      onChange={changeHandler}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label>Content</label>
+                  </div>
+                  <div className={style.TextArea}>
+                    <textarea
+                      id="content"
+                      name="content"
+                      placeholder="Input post content"
+                      value={newPostInfo.content}
+                      rows="6"
+                      onChange={changeHandler}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <input
+                    type="submit"
+                    value="Submit"
+                    onClick={createPost}
+                    disabled={isPostValid}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </Modal>
+      )}
+    </>
+  );
+};
 
-export default FavoritePosts
+export default FavoritePosts;
--- Job/User_posts_on_react/front/src/components/UserPosts/UserPosts.jsx
+++ Job/User_posts_on_react/front/src/components/UserPosts/UserPosts.jsx
@@ -1,130 +1,143 @@
-import React, {useContext, useEffect, useState} from 'react'
-import { PostsContext } from '../../context/postsContext'
-import Post from './../Post/Post'
-import Modal from '../ModalWindow/Modal'
-import style from './UserPosts.module.css'
-import { ModalContext } from '../../context/modalPostInfoContext'
-import { CurrentPostContext } from '../../context/currentPostContext'
-import { connect } from 'react-redux'
-import addInfo from './../../actionCreator/addInfo'
-import { ModalAddPostContext } from '../../context/modalAddInfoContext'
-import * as firebase from 'firebase'
+import React, { useContext, useEffect, useState } from "react";
+import { PostsContext } from "../../context/postsContext";
+import Post from "./../Post/Post";
+import Modal from "../ModalWindow/Modal";
+import style from "./UserPosts.module.css";
+import { ModalContext } from "../../context/modalPostInfoContext";
+import { CurrentPostContext } from "../../context/currentPostContext";
+import { connect } from "react-redux";
+import addInfo from "./../../actionCreator/addInfo";
+import { ModalAddPostContext } from "../../context/modalAddInfoContext";
+import * as firebase from "firebase";
 
-const UserPosts = ({addInfo}) => {
-    const { posts } = useContext(PostsContext)
-    const { toggleModal, isModalOpen } = useContext(ModalContext)
-    const { state } = useContext(CurrentPostContext)
-    const { isAddPostModalOpen, toggleAddPostModalOpen } = useContext(ModalAddPostContext)
-    
-    useEffect(() => {
-        posts.map(post => addInfo(false, post.id))
-    },[posts])
+const UserPosts = ({ addInfo }) => {
+  const { posts } = useContext(PostsContext);
+  const { toggleModal, isModalOpen } = useContext(ModalContext);
+  const { state } = useContext(CurrentPostContext);
+  const { isAddPostModalOpen, toggleAddPostModalOpen } = useContext(
+    ModalAddPostContext
+  );
 
-    let initialState = {
-        title: '',
-        content: '',
-    }
+  useEffect(() => {
+    posts.map((post) => addInfo(false, post.id));
+  }, [posts]);
 
-    const validStatus = true
-    const loaderState = false
+  let initialState = {
+    title: "",
+    content: "",
+  };
 
-    const [newPostInfo, setNewPostInfo] = useState(initialState)
-    const [isPostValid, setPostValid] = useState(validStatus)
-    const [isLoader, setLoader] = useState(loaderState)
+  const validStatus = true;
+  const loaderState = false;
+  const successState = false;
 
-    const changeHandler = (e) => {
-        setNewPostInfo({...newPostInfo, [e.target.id]:e.target.value})
-        newPostInfo.title.length > 1 && newPostInfo.content.length > 1 ? setPostValid(false) : setPostValid(true)
-    }
+  const [newPostInfo, setNewPostInfo] = useState(initialState);
+  const [isPostValid, setPostValid] = useState(validStatus);
+  const [isLoader, setLoader] = useState(loaderState);
+  const [isSuccess, setSuccess] = useState(successState);
 
-    const createPost = () => {
-        (async () => {
-            setLoader(true)
-            let db = await firebase.database()
-            let ref = await db.ref('server/saving-data/user-posts')
-            ref.set({newPostInfo})
-            setLoader(false)
-        })()
-        
-        setNewPostInfo(initialState)
-        setPostValid(validStatus)
-    }
+  const changeHandler = (e) => {
+    setNewPostInfo({ ...newPostInfo, [e.target.id]: e.target.value });
+    newPostInfo.title.length > 1 && newPostInfo.content.length > 1
+      ? setPostValid(false)
+      : setPostValid(true);
+  };
 
-    return (
-        <>
-            <div className={style.Container}>
-                {
-                    posts.map (post => {
-                        return (<Post post={post} key={post.id} id={post.id}/>)
-                    })
-                }
-            </div> 
-            { isModalOpen &&
-                <Modal>
-                    <div className={style.ModalContainer}>
-                        <button className={style.ModalButton} onClick={toggleModal}>x</button>
-                        <h2 className={style.ModalTitle}>{state.title}</h2>
-                        <div className={style.ModalContent}>{state.content}</div>
-                    </div>
-                </Modal>
-            }
-            { isAddPostModalOpen &&
-                <Modal>
-                    <div className={style.ModalContainer}>
-                        <button className={style.ModalButton} onClick={toggleAddPostModalOpen}>x</button>
-                        {isLoader ? (
-                            <div className={style.Loader}></div>
-                        ) : (
-                            <div className={style.Content}>
-                                <div>
-                                    <div>
-                                        <label>Title</label>
-                                    </div>
-                                <div>
-                                    <input 
-                                        type="text" 
-                                        id="title" 
-                                        name="title"
-                                        value={newPostInfo.title} 
-                                        placeholder="Input post title" 
-                                        onChange={changeHandler}
-                                        required
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <label>Content</label>
-                                </div>
-                                <div className={style.TextArea}>
-                                    <textarea 
-                                        id="content" 
-                                        name="content" 
-                                        placeholder="Input post content"
-                                        value={newPostInfo.content} 
-                                        rows="6" 
-                                        onChange={changeHandler}
-                                        required 
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <input 
-                                    type="submit" 
-                                    value="Submit" 
-                                    onClick={createPost}
-                                    disabled={isPostValid}
-                                />
-                            </div>
-                        </div>
-                        )
-                        }
-                    </div>
-                </Modal>
-            }
-         </>
-    )
-}
+  const createPost = async () => {
+    setLoader(true);
+    let db = await firebase.database();
+    let ref = await db.ref("server/saving-data/user-posts");
+    ref.set({ newPostInfo });
+    setSuccess(true)
+    setNewPostInfo(initialState);
+    setPostValid(validStatus);
+  };
 
-export {UserPosts}
-export default connect(null,{addInfo})(UserPosts)
+  return (
+    <>
+      <div className={style.Container}>
+        {posts.map((post) => {
+          return <Post post={post} key={post.id} id={post.id} />;
+        })}
+      </div>
+      {isModalOpen && (
+        <Modal>
+          <div className={style.ModalContainer}>
+            <button className={style.ModalButton} onClick={toggleModal}>
+              x
+            </button>
+            <h2 className={style.ModalTitle}>{state.title}</h2>
+            <div className={style.ModalContent}>{state.content}</div>
+          </div>
+        </Modal>
+      )}
+      {isAddPostModalOpen && (
+        <Modal>
+          <div className={style.ModalContainer}>
+            <button
+              className={style.ModalButton}
+              onClick={toggleAddPostModalOpen}
+            >
+              x
+            </button>
+            {isLoader ? (
+              <div>
+                {isSuccess ? (
+                  <h1>Data successfully saved!</h1>
+                ) : (
+                  <div className={style.Loader}></div>
+                )}
+              </div>
+            ) : (
+              <div className={style.Content}>
+                <div>
+                  <div>
+                    <label>Title</label>
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      id="title"
+                      name="title"
+                      value={newPostInfo.title}
+                      placeholder="Input post title"
+                      onChange={changeHandler}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label>Content</label>
+                  </div>
+                  <div className={style.TextArea}>
+                    <textarea
+                      id="content"
+                      name="content"
+                      placeholder="Input post content"
+                      value={newPostInfo.content}
+                      rows="6"
+                      onChange={changeHandler}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <input
+                    type="submit"
+                    value="Submit"
+                    onClick={createPost}
+                    disabled={isPostValid}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export { UserPosts };
+export default connect(null, { addInfo })(UserPosts);
